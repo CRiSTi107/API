f1b4e81afe71da0876d8f12df2534aed141b5442
Cristian <cvoiculescu@icloud.com>
2021-07-18 21:08:35 +0300

    Added xunit tests

---
+++ test/UserControllerTest.cs
@@ -0,0 +1,24 @@
+using Api.Controllers;
+using Api.Data;
+using Api.Service;
+using Api.Helpers;
+using Microsoft.Extensions.Options;
+using AutoMapper;
+
+namespace test
+{
+    public class UserControllerTest
+    {
+        UsersController usersController;
+
+        public UserControllerTest()
+        {
+            IUserRepository repository = new UserMockRepository();
+            IUserService service = new UserService(repository, );
+
+            usersController = new UsersController(repository, service, IMapper);
+
+
+        }
+    }
+}
